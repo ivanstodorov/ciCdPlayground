055852db9d80b740aa9449e4efba507879a1a508
Ivan Todorov <ivan.s.t@icloud.com>
2025-05-16 14:48:19 +0300

    Changed name

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "Christof Happ",
+    name: "Ivan Todorov",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
